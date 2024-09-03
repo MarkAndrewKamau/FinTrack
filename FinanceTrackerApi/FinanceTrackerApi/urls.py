14fe6ec25281f6abfc0c751b5df3eb76f5c0655f
Mark-andrew7 <kamaumark19@gmail.com>
2024-09-03 12:09:04 +0300

    Set urls

--- FinanceTrackerApi/FinanceTrackerApi/urls.py
+++ FinanceTrackerApi/FinanceTrackerApi/urls.py
@@ -23,9 +23,3 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-]
