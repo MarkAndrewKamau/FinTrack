06ee6852ebd1f710837e93e0417b172b605dabbd
JosephNjorog <njorojoe11173@gmail.com>
2024-09-05 20:10:54 +0300

    Update React Router import in App.js

--- FinanceTrackerApi/frontend/finance-tracker-frontend/src/App.js
+++ FinanceTrackerApi/frontend/finance-tracker-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './pages/Homepage';
@@ -11,22 +11,20 @@ import ProfilePage from './pages/ProfilePage';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/expenses" element={<ExpensesPage />} />
-            <Route path="/income" element={<IncomePage />} />
-            <Route path="/budget" element={<BudgetPage />} />
-            <Route path="/report" element={<ReportPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <div className="App">
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/expenses" element={<ExpensesPage />} />
+          <Route path="/income" element={<IncomePage />} />
+          <Route path="/budget" element={<BudgetPage />} />
+          <Route path="/report" element={<ReportPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
--- FinanceTrackerApi/frontend/finance-tracker-frontend/src/index.js
+++ FinanceTrackerApi/frontend/finance-tracker-frontend/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Import the new root API
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
+// Create the root element and render the App component
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <Router>
+    <Router> {/* Wrap the App component with BrowserRouter */}
       <App />
     </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
