5e08413f872cd94a7d8de683a7958659e473cd02
jxkimathi <judescriva8@gmail.com>
2024-09-10 15:11:15 +0300

    Change import on line 3 of models and remove unused import statement in models.py

--- FinanceTrackerApi/FinanceApp/models.py
+++ FinanceTrackerApi/FinanceApp/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.models.signals import post_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
-from
 
 class Expense(models.Model):
     """Model for Expense Tracking"""
@@ -76,7 +75,7 @@ class FinancialReport(models.Model):
 class Profile(models.Model):
     """Model for the profile"""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.Textfield(max_length=350, blank=True)
+    bio = models.TextField(max_length=350, blank=True)
     location = models.CharField(max_length=50, blank=True)
     birth_date = models.DateField(null=True, blank=True)
 
