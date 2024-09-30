30c42baba0fd4f41f0d0a436070b3c1fb8be78a4
jxkimathi <judescriva8@gmail.com>
2024-09-30 13:56:53 +0300

    Add Swagger API Documentation to settings.py and urls.py

--- FinanceTrackerApi/FinanceTrackerApi/urls.py
+++ FinanceTrackerApi/FinanceTrackerApi/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include 
+from django.urls import path, include, re_path
+from rest_framework import permissions
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework.routers import DefaultRouter
 from FinanceApp.views import ExpenseViewSet, IncomeViewSet, BudgetViewSet, FinancialReportListCreateAPIView, FinancialReportDetailAPIView, RegistrationView, UserViewSet, ProfileViewSet, NotificationListView
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
@@ -28,6 +31,19 @@ router.register(r'expenses', ExpenseViewSet, basename='expense')
 router.register(r'incomes', IncomeViewSet, basename='income')
 router.register(r'budgets', BudgetViewSet, basename='budget')
 router.register(r'profiles', ProfileViewSet, basename='profile')
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="FinTrack API",
+      default_version='v1',
+      description="API documentation for the FinTrack application",
+      terms_of_service="https://www.yourapp.com/terms/",
+      contact=openapi.Contact(email="support@yourapp.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -42,6 +58,8 @@ urlpatterns = [
     path('api/auth/social/', include('allauth.socialaccount.urls')),
     path('accounts/', include('allauth.urls')),
     path('api/notifications/', NotificationListView.as_view(), name='notification-list'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if settings.DEBUG:
