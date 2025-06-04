ed3879fac0772cff86992764c54ed81bf9e5d8a5
Mark-andrew7 <kamaumark19@gmail.com>
2025-06-04 18:54:47 +0000

    Finalized backend for Render deployment with SQLite

--- FinanceTrackerApi/FinanceTrackerApi/settings.py
+++ FinanceTrackerApi/FinanceTrackerApi/settings.py
@@ -148,10 +148,10 @@ WSGI_APPLICATION = 'FinanceTrackerApi.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600,
+    )
 }
 
 
